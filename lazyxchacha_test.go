aa1871fccdcd04c5aad48efe60a4c946bbee8a7c
prongbang <prongbang@gmail.com>
2024-04-17 22:55:43 +0700

    initial: update test

--- lazyxchacha_test.go
+++ lazyxchacha_test.go
@@ -31,7 +31,10 @@ func BenchmarkEncrypt(b *testing.B) {
 	key, _ := lazyXchacha.RandomKey()
 	plaintext := `{"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIn0.rTCH8cLoGxAm_xw68z-zXVKi9ie6xJn9tnVWjd_9ftE"}`
 	for i := 0; i < b.N; i++ {
-		lazyXchacha.Encrypt(plaintext, key)
+		_, err := lazyXchacha.Encrypt(plaintext, key)
+		if err != nil {
+			b.Errorf("Error %s", err)
+		}
 	}
 }
 
@@ -41,6 +44,9 @@ func BenchmarkDecrypt(b *testing.B) {
 	key, _ := hex.DecodeString("e7de22e898b35cf5ed2c2339f702210429da35909c6e070af48e1c01e8a34d55")
 	ciphertext := "f6a1bd8e40d1ac130a53c85f8eb3ce5c8f524dd16b6c844eba81f40430dc3f43a20f19ce0ff9cac0fb552e945c9c9eb03eef3d3ec120b0ff17e8181e2e3d949b2eb44180b494d72a33d79d30a4de4130488aabfb2922f7265c7010ddf649a231856f1a8dccd57284a53230c79d16a732d38a48f9a1fab78e3dca7eff3d48bb848a3f04169cefbf021523dc6e62def880ffefcd1e4d"
 	for i := 0; i < b.N; i++ {
-		lazyXchacha.Decrypt(ciphertext, key)
+		_, err := lazyXchacha.Decrypt(ciphertext, key)
+		if err != nil {
+			b.Errorf("Error %s", err)
+		}
 	}
 }
